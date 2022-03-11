7d7dd48c16bf151d6a5e09ba69333a349cdc5016
unknown <mahmodulshahin665@gmail.com>
2022-03-11 08:44:53 +0600

    add more file

---
+++ js/banking-fake.js
@@ -0,0 +1,106 @@
+//example
+// function doubleIt(num) {
+//     const result = num * 2;
+//     return result;
+// }
+// const first = doubleIt(5);
+// const second = doubleIt(7);
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    //clear deposit input after deposit btn is clicked
+    inputField.value = '';
+
+    return amountValue;
+}
+function updateTotalField(totalFieldId, amount) {
+    // debugger;
+    const totalElement = document.getElementById(totalFieldId);
+    const totalText = totalElement.innerText;
+    const previousTotal = parseFloat(totalText);
+    //calculate
+    totalElement.innerText = previousTotal + amount;
+}
+function getCurrentBalance() {
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+}
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    /*  const balanceTotalText = balanceTotal.innerText;
+     const previousBalanceTotal = parseFloat(balanceTotalText);  */
+    const previousBalanceTotal = getCurrentBalance();
+    if (isAdd == true) {
+        balanceTotal.innerText = previousBalanceTotal + amount;
+    }
+    else {
+        balanceTotal.innerText = previousBalanceTotal - amount;
+    }
+}
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // const depositInput = document.getElementById('deposit-input');
+    // const depositAmountText = depositInput.value;
+    // const depositAmount = parseFloat(depositAmountText);
+
+
+
+    //get and update deposit total
+    /*  const depositTotal = document.getElementById('deposit-total');
+     const depositTotalText = depositTotal.innerText;
+     const previousDepositTotal = parseFloat(depositTotalText);
+     //calculate
+     depositTotal.innerText = previousDepositTotal + depositAmount; */
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+
+
+
+    //update balance after deposit
+    /*  const balanceTotal = document.getElementById('balance-total');
+     const balanceTotalText = balanceTotal.innerText;
+     const previousBalanceTotal = parseFloat(balanceTotalText);
+     balanceTotal.innerText = previousBalanceTotal + depositAmount; */
+
+})
+
+//handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // const withdrawInput = document.getElementById('withdraw-input');
+    // const withdrawAmountText = withdrawInput.value;
+    // const withdrawAmount = parseFloat(withdrawAmountText);
+
+
+    // get and update withdraw total
+    /*const withdrawTotal = document.getElementById('withdraw-total');
+    const previouswithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previouswithdrawTotalText); 
+    
+     //calculate
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;*/
+
+
+
+    //update balance after withdraw                    
+    /*  const balanceTotal = document.getElementById('balance-total');
+     const balanceTotalText = balanceTotal.innerText;
+     const previousbalanceTotal = parseFloat(balanceTotalText);
+ 
+     balanceTotal.innerText = previousbalanceTotal - withdrawAmount; */
+    const withdrawAmount = getInputValue('withdraw-input')
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        updateTotalField('withdraw-total', withdrawAmount)
+        updateBalance(withdrawAmount, false);
+    }
+    if (withdrawAmount > currentBalance) {
+        alert('Insufficient Balance')
+    }
+
+
+})

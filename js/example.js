681dd37249f138d48803fce70b35e3934c5a0eb7
unknown <mahmodulshahin665@gmail.com>
2022-03-11 09:43:05 +0600

    add more file

---
+++ js/example.js
@@ -0,0 +1,7 @@
+
+function add() {
+    const cbalance = document.getElementById("c-balance").innerText;
+    console.log(cbalance)
+    const inputValue = document.getElementById("input").value;
+    console.log(cbalance + inputValue)
+}
